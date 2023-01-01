b5f88540d52bf320ff737e0c0593e6dd40a368bd
alainmigabo <alainmigabo2k03@gmail.com>
2023-01-01 10:49:58 -0800

    commit including attached calculator

--- calcscript.js
+++ calcscript.js
@@ -1,7 +1,24 @@
+let calculated = 0;
 let screen = document.forms[0]["input"];
 function entervalue(clicked){
-    if (screen.value == 0 && clicked.id != "minus" && clicked.id != "dot" && clicked.id != "plus" && clicked.id != "times" && clicked.id != "division") {
+    let countdots = 0;
+    for(i=0;i<screen.value.length;i++){
+        if (screen.value[i] == ".") {
+            countdots+=1;
+        }
+    }
+    if (calculated > 0) {
         screen.value = document.getElementById(clicked.id).innerHTML;
+        calculated = 0;
+    }
+    else if (screen.value == 0 && clicked.id != "minus" && clicked.id != "dot" && clicked.id != "plus" && clicked.id != "times" && clicked.id != "division") {
+        screen.value = document.getElementById(clicked.id).innerHTML;
+    }
+    else if (countdots > 0 && clicked.id == "dot") {
+        screen.value+="";
+    }
+    else if (clicked.id == "minus") {
+        screen.value+="-";
     }
     else{
         screen.value+=document.getElementById(clicked.id).innerHTML;
@@ -21,4 +38,14 @@ document.getElementById("input").onclick = function fun() {
     if (screen.value == 0) {
         screen.value = ""
     }
+}
+function calculate() {
+    input = screen.value;
+    for(i=0;i<input.length;i++){
+        if (input[i] == "X") {
+            input = input.replace("X","*")
+        }
+    }
+    screen.value = eval(input);
+    calculated+=1;
 }
